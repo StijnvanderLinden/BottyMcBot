ea04061f04657d5c45df1d09348149aaec568a8f
bryan <b.eggels@student.fontys.nl>
2017-11-28 16:11:20 +0100

    google api dingen

---
+++ src/main/java/GoogleAPI.java
@@ -0,0 +1,27 @@
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class GoogleAPI {
+
+
+    public GoogleAPI() {
+    }
+
+
+    public String Call() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://www.googleapis.com/customsearch/v1?key=AIzaSyDXZcf749KFywTPbBH6TgNJe1c_GVRqkFU&cx=017576662512468239146%3Aomuauf_lfve&q=lectures")
+                .get()
+                .addHeader("cache-control", "no-cache")
+
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return null;
+    }
+}
