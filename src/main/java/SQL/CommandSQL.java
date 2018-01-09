19846de365342c2679a61f6c15af8813a1d5cd77
bryan <b.eggels@student.fontys.nl>
2018-01-09 13:25:39 +0100

    dingen gedaan

--- src/main/java/SQL/CommandSQL.java
+++
@@ -1,9 +0,0 @@
-package SQL;
-
-import Interfaces.CommandDAO;
-
-/**
- * Created by Stijn on 19-12-2017.
- */
-public class CommandSQL implements CommandDAO {
-}
--- src/main/java/SQL/UserSQL.java
+++
@@ -1,9 +0,0 @@
-package SQL;
-
-import Interfaces.UserDAO;
-
-/**
- * Created by Stijn on 19-12-2017.
- */
-public class UserSQL implements UserDAO {
-}
